5b1f40414e697619e1371fd147044d6b72859932
Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch>
2006-09-25 09:57:06 +0000

    more options added

--- test/branchSizes.cpp
+++ test/branchSizes.cpp
@@ -38,55 +38,73 @@ static const char * const kSavePlotOpt ="save-plot";
 static const char * const kSavePlotCommandOpt ="save-plot,s";
 static const char * const kPlotTopOpt ="plot-top";
 static const char * const kPlotTopCommandOpt ="plot-top,t";
+static const char * const kVerboseOpt = "verbose";
+static const char * const kVerboseCommandOpt = "verbose,v";
+static const char * const kAlphabeticOrderOpt ="alphabetic-order";
+static const char * const kAlphabeticOrderCommandOpt ="alphabetic-order,A";
 
 typedef pair<size_t, size_t> size_type;
 
-size_type GetTotalSize( TBranch * );
+typedef pair<string, size_type> BranchRecord;
 
-size_type GetBasketSize( TBranch * );
+typedef vector<BranchRecord> BranchVector;
 
-size_type GetBasketSize( TObjArray * branches ) {
+ostream & operator << ( ostream & out, const size_type & s ) {
+  out << s.first << '/' << s.second << " bytes";
+  if ( s.second != 0 ) 
+    out << " ( compr: " << double( s.first ) / double ( s.second ) << ")";
+  return out;
+}
+
+size_type & operator+=( size_type & s1, const size_type & s2 ) {
+  s1.first += s2.first;
+  s1.second += s2.second;
+  return s1;
+}
+
+size_type GetTotalSize( TBranch *, bool verbose );
+
+size_type GetBasketSize( TBranch *, bool verbose );
+
+size_type GetBasketSize( TObjArray * branches, bool verbose ) {
   size_type result = make_pair( 0, 0 );
   size_t n = branches->GetEntries();
   for( size_t i = 0; i < n; ++ i ) {
     TBranch * b = dynamic_cast<TBranch*>( branches->At( i ) );
     assert( b != 0 );
-    size_type size = GetBasketSize( b );
-    result.first += size.first;
-    result.second += size.second;
+    result += GetBasketSize( b, verbose );
   }
   return result;
 }
 
-size_type GetBasketSize( TBranch * b ) {
+size_type GetBasketSize( TBranch * b, bool verbose ) {
   size_type result = make_pair( 0, 0 );
   if ( b != 0 ) {
     if ( b->GetZipBytes() > 0 ) {
-      result.first = b->GetTotBytes();
-      result.second = b->GetZipBytes();
+      result = make_pair( b->GetTotBytes(), b->GetZipBytes() );
     }
-    size_type size = GetBasketSize( b->GetListOfBranches() );
-    result.first += size.first;
-    result.second += size.second;
+    if ( verbose )
+      cout << " branch: " << b->GetName() << ", size:" << result.first << "/" << result.second << endl;
+    result += GetBasketSize( b->GetListOfBranches(), verbose );
   }
   return result;
 }
 
-size_type GetTotalSize( TBranch * br ) {
+size_type GetTotalSize( TBranch * br, bool verbose ) {
   TBuffer buf( TBuffer::kWrite, 10000 );
   TBranch::Class()->WriteBuffer( buf, br );
-  size_type size = GetBasketSize( br );
+  size_type size = GetBasketSize( br, verbose );
   size.first += buf.Length();
+  if ( verbose )
+    cout << ">>> total branch size: " << br->GetName() << ":" << size.first << "/" << size.second << endl;  
   return size;
 }
 
-size_type GetTotalSize( TObjArray * branches ) {
+size_type GetTotalSize( TObjArray * branches, bool verbose ) {
   size_type result = make_pair( 0, 0 );
   size_t n = branches->GetEntries();
   for( size_t i = 0; i < n; ++ i ) {
-    size_type size = GetTotalSize( dynamic_cast<TBranch*>( branches->At( i ) ) );
-    result.first += size.first;
-    result.second += size.second;
+    result += GetTotalSize( dynamic_cast<TBranch*>( branches->At( i ) ), verbose );
   }
   return result;
 }
@@ -99,18 +117,23 @@ size_type GetTotalSize( TTree *t ) {
   return make_pair( total, t->GetZipBytes() );
 } 
 
-size_type GetTotalBranchSize( TTree *t ) {
-  return GetTotalSize( t->GetListOfBranches() );
+size_type GetTotalBranchSize( TTree *t, bool verbose ) {
+  return GetTotalSize( t->GetListOfBranches(), verbose );
 } 
 
-template<typename T>
 struct sortByCompressedSize {
-  bool operator()( const T & t1, const T & t2 ) const {
+  bool operator()( const BranchRecord & t1, const BranchRecord & t2 ) const {
     size_t s1 = t1.second.second, s2 = t2.second.second;
     if ( s1 == 0 && s2 == 0 ) {
       s1 = t1.second.first; s2 = t2.second.first;
     }
     return s1 > s2;
+  }
+};
+
+struct sortByName {
+  bool operator()( const BranchRecord & t1, const BranchRecord & t2 ) const {
+    return t1.first < t2.first;
   }
 };
 
@@ -128,9 +151,11 @@ int main( int argc, char * argv[] ) {
     ( kHelpCommandOpt, "produce help message" )
     ( kAutoLoadCommandOpt, "automatic library loading (avoid root warnings)" )
     ( kDataFileCommandOpt, value<string>(), "data file" )
-    ( kPlotCommandOpt, value<string>(), "summary plot" )
+    ( kAlphabeticOrderCommandOpt, "sort by alphabetic order (default: sort by size)" )
+    ( kPlotCommandOpt, value<string>(), "produce a summary plot" )
     ( kPlotTopCommandOpt, value<int>(), "plot only the <arg> top size branches" )
-    ( kSavePlotCommandOpt, value<string>(), "save plot into root file <arg>" );
+    ( kSavePlotCommandOpt, value<string>(), "save plot into root file <arg>" )
+    ( kVerboseCommandOpt, "verbose printout" );
 
   positional_options_description p;
 
@@ -188,7 +213,8 @@ int main( int argc, char * argv[] ) {
     return 7004;
   }
 
-  typedef vector<pair<string, size_type> > BranchVector;
+  bool verbose = vm.count( kVerboseOpt ) > 0;
+
   BranchVector v;
   const size_t n =  branches->GetEntries();
   cout << fileName << " has " << n << " branches" << endl;
@@ -197,10 +223,14 @@ int main( int argc, char * argv[] ) {
     assert( b != 0 );
     string name( b->GetName() );
     if ( name == "EventAux" ) continue;
-    size_type s = GetTotalSize( b );
+    size_type s = GetTotalSize( b, verbose );
     v.push_back( make_pair( b->GetName(), s ) );
   }
-  sort( v.begin(), v.end(), sortByCompressedSize<pair<string, size_type> >() );
+  if ( vm.count( kAlphabeticOrderOpt ) ) {
+    sort( v.begin(), v.end(), sortByName() );
+  } else {
+    sort( v.begin(), v.end(), sortByCompressedSize() );
+  }
   bool plot = ( vm.count( kPlotOpt ) > 0 );
   bool save = ( vm.count( kSavePlotOpt ) > 0 );
   int top = n;
@@ -210,18 +240,16 @@ int main( int argc, char * argv[] ) {
   int x = 0;
   TAxis * cxAxis = compressed.GetXaxis();
   TAxis * uxAxis = uncompressed.GetXaxis();
+
   for( BranchVector::const_iterator b = v.begin(); b != v.end(); ++ b ) {
-    string name = b->first.c_str();
-    size_type s = b->second;
-    double cx = s.second == 0 ? 0 : double( s.first ) / double( s.second );
-    cout << s.second << "/" << s.first << " bytes ";
-    if ( cx != 0 ) cout << "(" << cx << ") ";
-    cout << b->first << endl;
+    const string & name = b->first;
+    size_type size = b->second;
+    cout << size << " " << name << endl;
     if ( x < top ) {
       cxAxis->SetBinLabel( x + 1, name.c_str() );
       uxAxis->SetBinLabel( x + 1, name.c_str() );
-      compressed.Fill( x, s.second );
-      uncompressed.Fill( x, s.first );
+      compressed.Fill( x, size.second );
+      uncompressed.Fill( x, size.first );
       x++;
     }
   }
